603a2df6ade0e66b1e7fa48ee0f3b2adb95811a9
hosseinhayati128 <42513489+hosseinhayati128@users.noreply.github.com>
2021-02-16 18:41:44 +0330

    Update image_paper_V1_3.m

--- image_paper_V1_3.m
+++ image_paper_V1_3.m
@@ -1,6 +1,5 @@
 % Name : hossein hayati 
 
-
 clc;
 clear;
 close all;
@@ -9,18 +8,17 @@ close all;
 
 % just load and plot an image for test
 sample_img = imread('C:\Users\h256.DESKTOP-NJDT39C\Documents\projects\azad\image\DS\distorted_images\I19_17_4.bmp');
-figure('Name','input image')
-imshow(sample_img)
+% sample_img = imread('C:\Users\h256.DESKTOP-NJDT39C\Documents\projects\azad\image\DS\reference_images\I19.bmp');
+
 sample_img = im2double(sample_img);
+figure('Name','input image double')
+imshow(sample_img)
+figure('Name','input image histogram')
+imhist(sample_img)
 
 % constants
 L = 256; % The magnitude of each and every color channel is confined within the range [0 , L-1]
 M = 256;% total intensity levels ???? ( wrong ) 
-
-% Extract color channels. REPEATED
-redChannel = sample_img(:,:,1); % Red channel
-greenChannel = sample_img(:,:,2); % Green channel
-blueChannel = sample_img(:,:,3); % Blue channel
 
 
 %%%%% 3. Proposed work %%%%%
@@ -56,10 +54,10 @@ g_stretched = gn/max_gn;
 before_stretched = (blueChannel+greenChannel+redChannel)/3;
 after_stretched = (b_stretched+r_stretched+g_stretched)/3;
 % close all
-figure('Name','imhist before_stretched')
-imhist(before_stretched);
-figure('Name','imhist after_stretched')
-imhist(after_stretched);
+% figure('Name','imhist before_stretched')
+% imhist(before_stretched);
+% figure('Name','imhist after_stretched')
+% imhist(after_stretched);
 % Recombine separate color channels into an RGB image.
 rgb_stretched_Image = cat(3, r_stretched, g_stretched, b_stretched);
 
@@ -72,9 +70,9 @@ hsi_image = rgb2hsi(rgb_stretched_Image);
 
 intensity = hsi_image(:, :, 3);
 
-figure('Name','imhist intensity')
+% figure('Name','imhist intensity')
 [counts , binLocations] = imhist(intensity);
-imhist(intensity);
+% imhist(intensity);
 hist = counts;
 
 
@@ -94,73 +92,54 @@ for hist_id = 1:length_hist
     end
     clipped_hist(hist_id) = Tc;
 end
-
-figure('Name','imhist hist')
-plot(hist)
-figure('Name','imhist clipped_hist')
-plot(clipped_hist)
+% 
+% figure('Name','imhist hist')
+% plot(hist)
+% figure('Name','imhist clipped_hist')
+% plot(clipped_hist)
 
 %%
 
 % (15) the corresponding PDF (p(i)) is calculated as:
 % M is the total intensity levels   
+P = clipped_hist / sum(clipped_hist);  %         QQQ
+% P = clipped_hist / M;
 
-% Pi = clipped_hist / M;
-tpi = zeros(1,length_hist);
+% (14) the CDF, c(i) is formulated as:
+C = cumsum(P);
 
-for i = 1:L
-    P(i) = clipped_hist(i) / sum(clipped_hist);
-end
-for i = 1:L
-    % (14) the CDF, c(i) is formulated as:
-    C(i) = sum(P(1:i));         
-end
-Pw = zeros(1,L);
+% (13) the corresponding Weighted Histogram Distribution function (WHD) is
+% constructed as:
+% [ Pmin,Pmax : min and max PDF of clipped histogram ]
+Pmin = min(P);
+Pmax = max(P);
+Pw = Pmax * power((P-Pmin)/(Pmax-Pmin),C);
 
-for i = 1:L
-    Alpha = C(i);
-
-    % (13) the corresponding Weighted Histogram Distribution function (WHD) is
-    % constructed as:
-    % [ Pmin,Pmax : min and max PDF of clipped histogram ]
-    
-    Pmin = min(P);
-    Pmax = max(P);
-
-    Pw(i) = Pmax * power((P(i)-Pmin)/(Pmax-Pmin),Alpha);
-end
 Cw = zeros(1,L);
 gamma = zeros(1,L);
-for i = 1:L
 
-%     plot(Pw(1:i))
+% (12) the weighted PDF sum is defined as follows:
+Sum_Pw = sum(Pw(1:L));
 
-    % (12) the weighted PDF sum is defined as follows:
-    %  [xL: because intensity is double between 0 and 1]  
-    % intensity_max = max(max(intensity*L)); 
-    intensity_max = max(max(intensity(1:i))); 
-%     Sum_Pw(i) = sum(Pw(2:intensity_max));
-%     Sum_Pw(i) = sum(Pw(1:intensity_max));
-    Sum_Pw = sum(Pw(1:L));
-   
-    % (11) the weighted CDF is defined as:
-    Cw(i) = sum(Pw(1:i)/Sum_Pw); 
+% (11) the weighted CDF is defined as:
+Cw = cumsum(Pw)/Sum_Pw;
 
-    % (10) the gamma parameter with weighted CDF is computed as:
-    gamma(i) = 1 - Cw(i);
-end
-% tpi = intlut(intensity,tpi_LUT);
-[length , width] = size(intensity) ;
+% (10) the gamma parameter with weighted CDF is computed as:
 
-result = zeros(length,width);
+gamma = 1 - Cw;
+[Length , Width] = size(intensity) ;
+result = zeros(Length,Width);
+intensity_max = max(intensity(:));
+% intensity_max = max(max(intensity(:))); 
+% intensity = uint8(intensity*255);
+
 
 % (9) Transformed pixel intensity 
-
 for i=1:L
     disp(i)
-    for l = 1:length
-        for w = 1:width
-            if uint8(intensity(l,w)*255) == i
+    for l = 1:Length
+        for w = 1:Width
+            if uint8(intensity(l,w)*255+1) == i
                result(l,w) =  power((intensity(l,w)/intensity_max),gamma(i));
             end
         end
@@ -172,6 +151,7 @@ H1 = hsi_image(:, :, 1);
 S1 = hsi_image(:, :, 2);
 I1 = result;
 result_rgb = hsi2rgb(H1,S1,I1);
+result_rgb = im2double(result_rgb);
 figure('Name','imhist rgb output')
 
 imhist(result_rgb)
