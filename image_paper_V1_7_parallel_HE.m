fc7ba6e1708ce0993dd037916e5185f80dbc7448
hosseinhayati128 <42513489+hosseinhayati128@users.noreply.github.com>
2021-02-16 21:42:58 +0330

    Create image_paper_V1_7_parallel_HE.m

---
+++ image_paper_V1_7_parallel_HE.m
@@ -0,0 +1,35 @@
+% Name : hossein hayati 
+
+
+clc;
+clear;
+close all;
+
+%%%%% Section 2 : Conventional image enhancement method %%%%%
+
+% just load and plot an image for test
+imagefiles = dir('C:\Users\h256.DESKTOP-NJDT39C\Documents\projects\azad\image\DS\distorted_images\*.bmp');      
+% imagefiles = dir('C:\Users\h256.DESKTOP-NJDT39C\Documents\projects\azad\image\DS\reference_images\*.bmp');      
+nfiles = length(imagefiles);  
+Sat_sum = 0;
+Contrast_sum = 0;
+Color_sum = 0;
+Entropy_sum = 0;
+
+for ii=1:nfiles
+   disp((ii/nfiles)*100)
+   currentfilename = imagefiles(ii).name;
+   img_folder = imagefiles(ii).folder;
+   sample_img = imread(fullfile(img_folder,currentfilename));
+   sample_img = im2double(sample_img);
+   HE_tst = histeq(sample_img);
+
+%%
+% 4.1 Entropy:
+
+Entropy = entropy(HE_tst);
+Entropy_sum = Entropy_sum + Entropy;
+
+end
+disp("Entropy")
+disp(Entropy_sum/nfiles)
