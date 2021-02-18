2fd223a489bb9e750e6a8b0689e5372fa65e76df
hosseinhayati128 <42513489+hosseinhayati128@users.noreply.github.com>
2021-02-18 12:00:55 +0330

    Update image_paper_V1_7_avg_HE.m

--- image_paper_V1_7_avg_HE.m
+++ image_paper_V1_7_avg_HE.m
@@ -5,11 +5,7 @@ clc;
 clear;
 close all;
 
-%%%%% Section 2 : Conventional image enhancement method %%%%%
-
-% just load and plot an image for test
 imagefiles = dir('C:\Users\h256.DESKTOP-NJDT39C\Documents\projects\azad\image\DS\distorted_images\*.bmp');      
-% imagefiles = dir('C:\Users\h256.DESKTOP-NJDT39C\Documents\projects\azad\image\DS\reference_images\*.bmp');      
 nfiles = length(imagefiles);  
 Sat_sum = 0;
 Contrast_sum = 0;
@@ -24,9 +20,7 @@ for ii=1:nfiles
    sample_img = im2double(sample_img);
    HE_tst = histeq(sample_img);
 
-%%
 % 4.1 Entropy:
-
 Entropy = entropy(HE_tst);
 Entropy_sum = Entropy_sum + Entropy;
 
